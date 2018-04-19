e9cf51fd8405cf2d2e1da622a6ef9a13e107e1af
Hackcracks <buklaisvas1@gmail.com>
2018-04-19 14:22:49 +0300

    JS duomenu suvedimas

---
+++ Projects/Uzduotis 04-19/js/script.js
@@ -0,0 +1,122 @@
+function add(){
+    alert("VEIKIA");
+}
+
+
+let product_array = [];
+
+
+let stored_products_string = localStorage.getItem("PRODUCTS");
+let stored_products = JSON.parse(stored_products_string);
+console.log(stored_products);
+
+if(stored_products != null){    
+    product_array = stored_products;
+}
+
+// printProducts(product_array);
+
+// function add(){
+    
+//     //nuskaityti reiksme nuo laukelio
+//     let newProduct = document.getElementById("prod").value;
+
+//     //patikrinti ar elementas egzistuoja
+//     let rasta_dublikatu = false; // boolean
+    
+//     rasta_dublikatu = checkForDuplicates(newProduct, product_array);
+
+//     if (rasta_dublikatu  ==  false) {
+//         product_array.push(newProduct);        
+//     } else {
+//         alert("Toki produkta jau irasete!");
+//     }   
+   
+//     // atvaizduoti masyva
+//     printProducts(product_array);
+
+//     // issaugot shopping lista
+//     let product_array_as_string = JSON.stringify(product_array);
+//     localStorage.setItem("PRODUCTS", product_array_as_string);
+
+//     //patikrinti ar leidziama mygtukui atsigaut
+
+// }
+
+// function printProducts(product_array){
+//     let h = "";
+//     h = h + "<ul>";
+//     for(let i = 0; i < product_array.length; i++) {        
+//         h = h + "<li>"+product_array[i] + "</li>";
+//     }
+//     h = h + " </ul>";   
+//     document.getElementById("result").innerHTML = h;
+// }
+
+
+
+// function checkForDuplicates(newProduct, product_array){
+//     let found = false;
+    
+//     for(let i = 0; i < product_array.length; i++){
+//         if(product_array[i] == newProduct){
+//             found = true;
+//         }        
+//     }
+
+//     return found;
+// }
+
+
+// function releaseButton(){
+//     document.getElementById("btn_add").disabled = false;
+// }
+
+
+
+
+
+print
+
+function add() {
+
+    let prod_text = document.getElementById("prod").value;
+
+    let price_text = document.getElementById("price").value;
+    let price_number = Number(price_text);
+
+    let qty_text = document.getElementById("qty").value;
+    let qty_number = Number(qty_text);
+
+    let city_text = document.getElementById("city").value;
+
+
+    //
+    let product = {
+        //products properties
+        prod: prod_text,
+        price: price_number,
+        qty: qty_number,
+        city: city_text
+    
+    //Products methods  OUTPUT
+    // print: function() {
+    //     console.log(product);
+    // }
+}
+}
+
+function printProducts(product){
+    let h = "";
+    h = h + "<table class='table table-striped' id=myTable>";
+    h = h + "<tr><thead class='table table-striped' id=myTable>";
+}
+
+<thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
